678ffe0b2fbf6f9cfe957423e2c0b5b2deb87383
shanley <sh11zuka@gmail.com>
2019-07-31 13:50:02 -0700

    Portfolio updated 07/31 v3

--- js/greetings.js
+++ js/greetings.js
@@ -1,7 +1,7 @@
 var d = new Date();
 var hour = d.getHours();
 
-if (hour>=6 && hour<12) {
+if (hour>=24 && hour<12) {
     document.getElementById("greetings").innerHTML = "Good Morning";
 }
 else if (hour>18 && hour<24) {
